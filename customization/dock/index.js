9854bc2f22ef418f959063db8c4965757cbca486
Jared Reynolds <jared.reynolds@changepoint.com>
2017-12-30 15:24:41 -0800

    Dock customization

---
+++ customization/dock/index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const orderedWhitelist = [
+    'Amazon Music',
+    'Microsoft Outlook',
+    'Slack',
+    'Microsoft OneNote',
+    'Producteev',
+    'Google Chrome',
+    'Firefox',
+    'iTerm',
+    'VirtualBox',
+    'Visual Studio Code',
+    'Postman'
+]
+
+const dockPlistPath = `${process.env.HOME}/Library/Preferences/com.apple.dock.plist`
+const fs = require('fs')
+const plist = require('simple-plist')
+
+// read existing dock plist
+const dock = plist.readFileSync(dockPlistPath)
+
+// whitelist apps and assemble them in order
+const orderedWhitelistedApps = Array(orderedWhitelist.length)
+for (const app of dock['persistent-apps']) {
+    const fileLabel = app['tile-data']['file-label']
+    const index = orderedWhitelist.indexOf(fileLabel)
+    if (index >= 0) {
+        orderedWhitelistedApps[index] = app
+    }
+}
+
+// replace the app list, excluding null apps
+dock['persistent-apps'] = orderedWhitelistedApps.filter(app => app)
+
+// backup and write out the new dock plist
+fs.copyFileSync(dockPlistPath, `${dockPlistPath}.bak`)
+plist.writeBinaryFileSync(dockPlistPath, dock)
